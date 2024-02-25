861fa94e3d2e5e48adc81c25d0bb8e648e3391cd
YunusovXumoyunDev <yunusovhumoyun13@gmail.com>
2024-02-26 01:03:06 +0500

    Login App

---
+++ app/src/main/java/uz/yxdev/loginapp/presenter/UserPresenter.kt
@@ -0,0 +1,10 @@
+package uz.yxdev.loginapp.presenter
+
+import uz.yxdev.loginapp.data.repository.AuthRepository
+import uz.yxdev.loginapp.screen.UserDetailScreen
+
+class UserPresenter(
+    private val view:UserDetailScreen,
+    private val repository: AuthRepository
+) {
+}
--- app/src/main/java/uz/yxdev/loginapp/screen/UserDetailScreen.kt
+++ app/src/main/java/uz/yxdev/loginapp/screen/UserDetailScreen.kt
@@ -6,18 +6,25 @@ import androidx.fragment.app.Fragment
 import uz.yxdev.loginapp.R
 import uz.yxdev.loginapp.data.repository.AuthRepository
 import uz.yxdev.loginapp.databinding.ScreenUserDetailBinding
-import uz.yxdev.loginapp.presenter.LoginPresenter
+import uz.yxdev.loginapp.presenter.UserPresenter
 
 class UserDetailScreen : Fragment(R.layout.screen_user_detail) {
     private var _binding: ScreenUserDetailBinding? = null
     private val binding: ScreenUserDetailBinding get() = _binding!!
-    private lateinit var presenter: LoginPresenter
+    private lateinit var presenter: UserPresenter
     private lateinit var repository: AuthRepository
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        repository= AuthRepository(requireContext())
+        presenter= UserPresenter(this,repository)
         _binding = ScreenUserDetailBinding.bind(view)
-
+        loadUi()
     }
-
+    private fun loadUi(){
+        binding.name.text=repository.getName()
+        binding.login.text=repository.getName()
+        binding.phone.text=repository.getName()
+        binding.email.text=repository.getName()
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding=null
